98dca413e65c52a9681a4e0db4efd2124df7d7ae
Sambo Chea <sombochea@cubetiqs.com>
2020-09-26 13:09:02 +0700

    updated build and remove some unused

--- main.js
+++ main.js
@@ -1,25 +1,34 @@
 const electron = require("electron");
 const { app, BrowserWindow } = electron;
 
-const DR_URL = process.env.DR_URL || "https://clinic.cubetiqs.com?platform=desktop&offline=true&source=" + (process.env.USERNAME || "unknown");
-const DR_TITLE = "CUBETIQ CLINIC"
+const URL = "https://app.drprofessionalglobal.com";
+
+const DR_URL =
+  process.env.DR_URL ||
+  URL +
+    "?platform=desktop&offline=true&source=" +
+    (process.env.USERNAME || "unknown");
+const DR_TITLE = "CUBETIQ CLINIC";
 
 let mainApp;
 app.allowRendererProcessReuse = true;
 app.on("ready", () => {
-
   mainApp = new BrowserWindow({
-    width: 1000,
-    height: 700
+    width: 1024,
+    height: 768,
   });
 
-  mainApp.webContents.executeJavaScript(`localStorage.setItem("author", "Sambo Chea <sombochea@cubetiqs.com>")`)
-  mainApp.webContents.executeJavaScript(`localStorage.setItem("IS_ELECTRON", true)`)
-  
-  mainApp.setFullScreen(true)
+  mainApp.webContents.executeJavaScript(
+    `localStorage.setItem("author", "Sambo Chea <sombochea@cubetiqs.com>")`
+  );
+  mainApp.webContents.executeJavaScript(
+    `localStorage.setItem("IS_ELECTRON", true)`
+  );
+
+  mainApp.setFullScreen(true);
   mainApp.setTitle(DR_TITLE);
 
-  console.log("Starting from:", DR_URL)
+  console.log("Starting from:", DR_URL);
   mainApp.loadURL(DR_URL);
 
   mainApp.on("closed", () => {
